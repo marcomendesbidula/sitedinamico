0be9e4ced361f41e2c5672c69c80bdc9d0b71475
Marco Aurelio Mendes <mendes_marcoaurelio@yahoo.com.br>
2022-08-02 15:54:24 -0300

    iniciando o projeto com HTML

--- index.php
+++ index.php
@@ -9,6 +9,57 @@
     <title>Site Dinamico</title>
 </head>
 <body>
-    
+    <header>
+        <div class="center">
+            <div class="logo">logomarca</div><!--logo-->
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div><!--center-->
+    </header>
+
+    <section class="banner-principal">
+        <div classs="center">
+        <form>
+            <h2>Qual é o seu e-mail?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="acao" value="cadastrar" />
+        </form>
+        </div><!--center-->
+    </section><!--banner-principal-->
+
+    <section class="descricao-autor">
+        <div class="center">
+        <div class="w-50">
+            <h2>Marco Aurélio M. Santos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, diam in sodales accumsan, sapien mi tincidunt libero, eget placerat quam risus in ante. Phasellus auctor molestie ipsum, quis vehicula sapien. Fusce aliquam in magna at tristique. Curabitur eu felis faucibus sem tempor aliquam id eget neque. Pellentesque sit amet tempus urna. Nullam sagittis pellentesque blandit.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra, diam in sodales accumsan, sapien mi tincidunt libero, eget placerat quam risus in ante. Phasellus auctor molestie ipsum, quis vehicula sapien. Fusce aliquam in magna at tristique. Curabitur eu felis faucibus sem tempor aliquam id eget neque. Pellentesque sit amet tempus urna. Nullam sagittis pellentesque blandit.</p>
+        </div><!--w50-->
+        <div class="">
+            <img src="imagens/foto-perfil.jpg" />
+        </div><!--w50-->
+        </div><!--center-->
+    </section><!--descricao-autor-->
+
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+
+        </div><!--center-->
+    </section>
+
 </body>
 </html>
